106ac9db2623f3ca3589fdc2917eebeb6d0c9976
Yoko Harada <yokolet@gmail.com>
2024-09-18 16:19:05 +0900

    adds images, screen size configs, and custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
     extend: {
+      colors: {
+        strongCyan: 'hsl(171, 66%, 44%)',
+        lightBlue: 'hsl(233, 100%, 69%)',
+        darkGrayishBlue: 'hsl(210, 10%, 33%)',
+        grayishBlue: 'hsl(201, 11%, 66%)',
+      },
       fontFamily: ['Bai Jamjuree', 'sans-serif'],
     },
   },
