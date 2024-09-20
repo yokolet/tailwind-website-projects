5a8bba4f18868a58f7053b46f6a285ca66448ecd
Yoko Harada <yokolet@gmail.com>
2024-09-20 20:31:50 +0900

    creates a footer

--- src/views/LoopStudio.vue
+++ src/views/LoopStudio.vue
@@ -54,6 +54,13 @@ const items = ref<IItem[]>([
     title: 'Make It Fisheye',
   },
 ]);
+
+const socialIcons = ref<string[]>([
+    '/src/assets/loopstudios/images/icon-facebook.svg',
+    '/src/assets/loopstudios/images/icon-twitter.svg',
+    '/src/assets/loopstudios/images/icon-pinterest.svg',
+    '/src/assets/loopstudios/images/icon-instagram.svg',
+])
 </script>
 
 <template>
@@ -161,6 +168,45 @@ const items = ref<IItem[]>([
         </div>
       </div>
     </section>
+    <!-- Footer -->
+    <footer class="bg-black">
+      <!-- Container -->
+      <div class="container max-w-6xl py-10 mx-auto">
+        <!-- Footer Flex Container -->
+        <div class="flex flex-col items-center mb-8 space-y-6 md:flex-row md:space-y-0 md:justify-between md:items-start">
+          <!-- Manu and Logo Container -->
+          <div class="flex flex-col items-center space-y-8 md:items-start md:space-y-4">
+            <!-- Logo -->
+            <div class="h-8">
+              <img src="../assets/loopstudios/images/logo.svg" alt="" class="w-44 md:ml-3" />
+            </div>
+            <!-- Menu Container -->
+            <div class="flex flex-col items-center space-y-4 font-bold text-white md:flex-row md:space-y-0 md:space-x-6 md:ml-3">
+              <div v-for="menu in menus" class="h-10 group">
+                <a href="#">{{ menu }}</a>
+                <div class="mx-2 group-hover:border-b group-hover:border-blue-50"></div>
+              </div>
+            </div>
+          </div>
+          <!-- Social & Copy Container -->
+          <div class="flex flex-col items-start justify-between space-y-4 text-gray-500">
+            <!-- icons Container -->
+            <div class="flex items-center justify-center mx-auto space-x-4 md:justify-end md:mx-0">
+              <!-- Icons -->
+              <div v-for="icon in socialIcons" class="h-8 group">
+                <a href="#">
+                  <img :src="icon" alt="" class="h-6">
+                </a>
+              </div>
+            </div>
+            <!-- Copy -->
+            <div class="font-bold">
+              &copy; 2024 Loopstudios. All rights reserved
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
