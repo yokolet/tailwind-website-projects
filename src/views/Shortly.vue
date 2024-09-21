12eecbf9a9c26f74dfb13fa2fcc57653e18f97dc
Yoko Harada <yokolet@gmail.com>
2024-09-21 18:25:22 +0900

    adds a url verification logic

--- src/views/Shortly.vue
+++ src/views/Shortly.vue
@@ -19,7 +19,32 @@ const urls = ref<IURL[]>([
     fullUrl: 'https://linkedin.com/frontend-mentor',
     shortUrl: 'https://rel.ink/gob3X9',
   }
-])
+]);
+
+const inputText = ref<string | null>(null);
+const errorMsg = ref<string>('');
+
+const validateURL = (url: string) => {
+  const regex = new RegExp('^(https?:\\/\\/)?' + // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+      '((\\d{1,3}\\.){3}\\d{1,3}))' +
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+      '(\\?[;&a-z\\d%_.~+=-]*)?' +
+      '(\\#[-a-z\\d_]*)?$',
+      'i');
+  return !!regex.test(url);
+}
+const verifyUrl = () => {
+  if (inputText.value === null || inputText.value === '') {
+    errorMsg.value = 'Please enter something';
+  } else if (inputText.value !== null && !validateURL(inputText.value)) {
+    errorMsg.value = 'Please enter a valid URL';
+  } else if (inputText.value !== null) {
+    errorMsg.value = '';
+    alert('Success');
+  }
+  inputText.value = '';
+}
 </script>
 
 <template>
@@ -95,16 +120,21 @@ const urls = ref<IURL[]>([
             id="link-form"
             class="relative flex flex-col w-full p-10 -mt-20 space-y-4 bg-darkViolet rounded-lg md:flex-row
             md:space-y-0 md:space-x-3"
+            @submit.prevent="verifyUrl"
         >
           <input
+              id="link-input"
               type="text"
               class="flex-1 p-3 border-2 rounded-lg placeholder-yellow-500 focus:outline-none"
+              :class="errorMsg.length > 0 ? 'border-red' : ''"
               placeholder="Shorten a link here"
-              id="link-input"
+              v-model="inputText"
           />
           <button class="px-10 py-3 text-white bg-cyan rounded-lg hover:bg-cyanLight focus:outline-none md:py-2">
             Shorten It!
           </button>
+          <!-- Error Messages -->
+          <div id="error-msg" class="absolute left-7 bottom-3 text-red text-sm italic">{{ errorMsg }}</div>
         </form>
         <!-- Links -->
         <div v-for="(url, index) in urls"
