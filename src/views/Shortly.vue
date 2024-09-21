a1fed5fdafa4fcc34f00fe5123fd2fbf232f3036
Yoko Harada <yokolet@gmail.com>
2024-09-21 16:10:10 +0900

    creates shorten form and result list

--- src/views/Shortly.vue
+++ src/views/Shortly.vue
@@ -1,5 +1,25 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 
+interface IURL {
+  fullUrl: string;
+  shortUrl: string;
+}
+
+const urls = ref<IURL[]>([
+  {
+    fullUrl: 'https://frontendmentor.io',
+    shortUrl: 'https://rel.ink/k4IKyk',
+  },
+  {
+    fullUrl: 'https://twitter.com/frontendmentor',
+    shortUrl: 'https://rel.ink/gxOXp9',
+  },
+  {
+    fullUrl: 'https://linkedin.com/frontend-mentor',
+    shortUrl: 'https://rel.ink/gob3X9',
+  }
+])
 </script>
 
 <template>
@@ -62,6 +82,45 @@
         <!-- Image -->
         <div class="mb-24 mx-auto md:w-180 lg:mb-0 lg:w-1/2">
           <img src="../assets/shortly/images/illustration-working.svg" alt="">
+        </div>
+      </div>
+    </section>
+
+    <!-- Shorten Section -->
+    <section id="shorten" class="relative bg-gray-100">
+      <!-- Shorten Container -->
+      <div class="max-w-4xl mx-auto p-6 space-y-6">
+        <!-- Form -->
+        <form
+            id="link-form"
+            class="relative flex flex-col w-full p-10 -mt-20 space-y-4 bg-darkViolet rounded-lg md:flex-row
+            md:space-y-0 md:space-x-3"
+        >
+          <input
+              type="text"
+              class="flex-1 p-3 border-2 rounded-lg placeholder-yellow-500 focus:outline-none"
+              placeholder="Shorten a link here"
+              id="link-input"
+          />
+          <button class="px-10 py-3 text-white bg-cyan rounded-lg hover:bg-cyanLight focus:outline-none md:py-2">
+            Shorten It!
+          </button>
+        </form>
+        <!-- Links -->
+        <div v-for="(url, index) in urls"
+            class="flex flex-col items-center justify-between w-full p-6 bg-white rounded-ld md:flex-row">
+          <p class="font-bold text-center text-veryDarkViolet md:text-left">
+            {{ url.fullUrl }}
+          </p>
+          <div class="flex flex-col items-center justify-end flex-1 space-x-4 space-y-2 md:flex-row md:space-y-0">
+            <div class="font-bold text-cyan">{{ url.shortUrl }}</div>
+            <button
+                :class="index % 2 == 1 ? 'bg-veryDarkViolet' : 'bg-cyan'"
+                class="p-2 px-8 text-white rounded-lg hover:opacity-70 focus:outline-none"
+            >
+              Copy
+            </button>
+          </div>
         </div>
       </div>
     </section>
