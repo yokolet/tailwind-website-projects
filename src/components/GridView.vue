0ea9c83ac962a6f7eab1418a56fdd728222b0343
Yoko Harada <yokolet@gmail.com>
2024-09-22 16:54:00 +0900

    improves home view

---
+++ src/components/GridView.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+</script>
+
+<template>
+<div class="bg-purple-100 my-auto md:min-h-screen w-screen p-6">
+  <div class="flex flex-col md:flex-row items-center justify-center mt-10">
+    <router-link to="/clipboard">
+      <div class="relative items-center justify-center h-64 w-64 bg-orange-400 p-10 hover:opacity-70 duration-200">
+        <div class="text-2xl text-bold text-center text-white capitalize">Clipboard</div>
+        <div class="absolute px-2 left-4 bottom-8 md:bottom-6 text-lg text-gray-100">
+          <div class="flex flex-row px-4">
+            Clipboard landing page
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            </svg>
+          </div>
+        </div>
+      </div>
+    </router-link>
+    <router-link to="/loop-studios">
+      <div class="relative items-center justify-center h-64 w-64 bg-yellow-500 p-10 hover:opacity-70 duration-200">
+        <div class="text-2xl text-bold text-center text-white capitalize">Loopstudios</div>
+        <div class="absolute px-2 left-4 bottom-8 md:bottom-6 text-lg text-gray-100">
+          <div class="flex flex-row px-4">
+            Loopstudios landing page
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            </svg>
+          </div>
+        </div>
+      </div>
+    </router-link>
+    <router-link to="/shortly">
+      <div class="relative items-center justify-center h-64 w-64 bg-pink-700 p-10 hover:opacity-70 duration-200">
+        <div class="text-2xl text-bold text-center text-white capitalize">Shortly</div>
+        <div class="absolute px-2 left-4 bottom-8 md:bottom-6 text-lg text-gray-100">
+          <div class="flex flex-row px-4">
+            Shortly landing page
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            </svg>
+          </div>
+        </div>
+      </div>
+    </router-link>
+    <router-link to="/testimonial-grid">
+      <div class="relative items-center justify-center h-64 w-64 bg-amber-600 p-10 hover:opacity-70 duration-200">
+        <div class="text-2xl text-bold text-center text-white capitalize">Testimonial Grid</div>
+        <div class="absolute px-2 left-4 bottom-8 md:bottom-6 text-lg text-gray-100">
+          <div class="flex flex-row px-4">
+            Testimonial Grid project
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            </svg>
+          </div>
+        </div>
+      </div>
+    </router-link>
+  </div>
+</div>
+</template>
+
+<style scoped>
+</style>
