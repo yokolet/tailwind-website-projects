3b2520fbbdd54ccbbc72c08174b22e25733a32f8
Yoko Harada <yokolet@gmail.com>
2024-09-23 13:15:33 +0900

    sets up a dark/light theme changer

--- src/views/Fylo.vue
+++ src/views/Fylo.vue
@@ -1,18 +1,23 @@
 <script setup lang="ts">
-import { useStorage } from '@vueuse/core';
+import { useDark, useToggle } from "@vueuse/core";
 
-const isDark = useStorage('fylo-dark-color-theme', true, sessionStorage);
-
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
 </script>
 
 <template>
-  <div id="app" :class="isDark ? 'dark' : ''">
-
+  <div id="website">
+    <button
+        @click="toggleDark()"
+        class="border border-gray-800 dark:border-gray-200 m-4 p-2 rounded-xl">
+      Toggle Mode to {{ isDark ? 'Light' : 'Dark' }}
+    </button>
+    <div class="text-3xl dark:text-yellow-600 text-pink-600 m-4">Tailwind CSS</div>
   </div>
 </template>
 
 <style scoped>
-#app {
-  @apply font-raleway;
+#website {
+  @apply h-screen w-screen font-opensans dark:bg-darkBlue dark:text-white;
 }
 </style>
