edd160b3ef94f61ba867e0b3e21682ad42e99f11
Yoko Harada <yokolet@gmail.com>
2024-09-23 21:29:10 +0900

    creates a footer and adds a smooth scroll setting

--- src/views/Fylo.vue
+++ src/views/Fylo.vue
@@ -123,7 +123,7 @@ const customers = ref<ICustomer[]>([
     <!-- Hero Section -->
     <section id="hero" class="bg-curvy-light-mode dark:bg-curvy-dark-mode bg-no-repeat bg-contain bg-bottom">
       <!-- Hero Container -->
-      <div class="container mx-auto px-6 text-center md:pt-20 pb-52">
+      <div class="container mx-auto px-6 text-center pt-10 md:pt-20 pb-52">
         <img src="../assets/fylo/images/illustration-intro.png" alt="" class="mx-auto">
         <h1 class="max-w-2xl mx-auto mb-10 text-3xl font-bold leading-normal mt-14 md:text-4xl">
           All your files in one secure location, accessible anywhere.
@@ -262,11 +262,70 @@ const customers = ref<ICustomer[]>([
         </div>
       </div>
     </section>
+    <!-- Footer -->
+    <footer class="bg-darkBlue2 text-white">
+      <div class="container mx-auto pt-12 px-5 pb-10">
+        <!-- Flex Container for All Items -->
+        <div class="flex flex-col justify-between space-y-24 md:flex-row md:space-y-0">
+          <!-- Email & Phone -->
+          <div class="mt-10 space-y-6">
+            <div class="flex items-center space-x-3 md:-mt-10">
+              <div class="w-6">
+                <img src="../assets/fylo/images/icon-phone.svg" alt="" class="scale-10">
+              </div>
+              <div>+1-543-123-4567</div>
+            </div>
+            <div class="flex items-center space-x-3">
+              <div class="w-6">
+                <img src="../assets/fylo/images/icon-email.svg" alt="" class="scale-10">
+              </div>
+              <div>example@fylo.com</div>
+            </div>
+          </div>
+          <! -- Menus -->
+          <div class="flex flex-col space-y-10 text-xl md:text-base md:space-x-20 md:space-y-0 md:flex-row">
+            <div class="flex flex-col space-y-3">
+              <a href="#">About</a>
+              <a href="#">Jobs</a>
+              <a href="#">Press</a>
+              <a href="#">Blog</a>
+            </div>
+            <div class="flex flex-col space-y-3">
+              <a href="#">Contact</a>
+              <a href="#">Terms</a>
+              <a href="#">Privacy</a>
+            </div>
+          </div>
+          <!-- Social -->
+          <div class="flex justify-center pb-10 space-x-3">
+            <div>
+              <a href="#">
+                <img src="../assets/fylo/images/facebook.svg" alt="" class="p-2 bg-darkBlue rounded-full ficon">
+              </a>
+            </div>
+            <div>
+              <a href="#">
+                <img src="../assets/fylo/images/twitter.svg" alt="" class="p-2 bg-darkBlue rounded-full ficon">
+              </a>
+            </div>
+            <div>
+              <a href="#">
+                <img src="../assets/fylo/images/instagram.svg" alt="" class="p-2 bg-darkBlue rounded-full ficon">
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
 <style scoped>
 #website {
   @apply min-h-screen w-screen font-opensans dark:bg-darkBlue dark:text-white;
+}
+
+.ficon:hover {
+  filter: invert(80%) sepia(37%) saturate(7048%) hue-rotate(132deg) brightness(105%) contrast(104%);
 }
 </style>
