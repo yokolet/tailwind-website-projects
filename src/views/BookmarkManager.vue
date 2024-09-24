3101c64ad2e88834d098985ff7f7a1095b224b8e
Yoko Harada <yokolet@gmail.com>
2024-09-24 22:50:36 +0900

    creates a newsletter form and footer

--- src/views/BookmarkManager.vue
+++ src/views/BookmarkManager.vue
@@ -330,6 +330,63 @@ const FAQs = ref<IFAQ[]>([
         </div>
       </div>
     </section>
+
+    <!-- News Letter Section -->
+    <section id="newsletter" class="bg-softBlue">
+      <!-- Main Container -->
+      <div class="max-w-lg mx-auto py-24">
+        <div class="mb-6 text-lg tracking-widest text-center text-white uppercase">
+          35,000+ Already Joined
+        </div>
+        <h2 class="px-3 mb-6 text-3xl font-semibold text-center text-white md:text-4xl">
+          Stay up-to-date with what we're doing
+        </h2>
+        <!-- Form -->
+        <form action=""
+              class="flex flex-col items-start justify-center max-w-2xl mx-auto space-y-6 text-base px-6
+              md:flex-row md:space-y-0 md:space-x-4 md:px-0">
+          <!-- Input & Button Container -->
+          <div class="flex flex-col justify-between items-center mx-auto mf:flex-row md:mx-0">
+            <input
+                type="text"
+                class="flex-1 px-6 pt-3 pb-2 mb-4 rounded-lg border-1 border-white focus:outline-none md:mr-3 md:mb-0"
+                placeholder="Enter your email address"
+            />
+            <input
+                type="submit"
+                class="inline-flex px-6 py-3 font-semibold text-center text-white duration-200 transform rounded-lg cursor-pointer focus:outline-none bg-softRed hover:opacity-90"
+                value="Contact Us"
+            />
+          </div>
+        </form>
+      </div>
+    </section>
+
+    <!-- Footer -->
+    <footer class="py-16 bg-veryDarkBlue2">
+      <!-- Footer Flex Container -->
+      <div class="container flex flex-col items-center justify-between mx-auto space-y-16 px-6 md:flex-row md:space-y-0">
+        <!-- Logo/Menu Container -->
+        <div
+            class="flex flex-col items-center justify-between space-y-8 text-lg font-light text-grayishBlue2
+            md:flex-row md:space-y-0 md:space-x-14"
+        >
+          <img src="../assets/bookmark/images/logo-bookmark-footer.svg" alt="" class="mb-1" />
+          <a href="#features" class="uppercase hover:text-softRed hover:text-softRed">Features</a>
+          <a href="#download" class="uppercase hover:text-softRed hover:text-softRed">Download</a>
+          <a href="#faq" class="uppercase hover:text-softRed hover:text-softREd">FAQ</a>
+        </div>
+        <!-- Social Container -->
+        <div class="flex space-x-10">
+          <a href="#">
+            <img src="../assets/bookmark/images/icon-facebook.svg" alt="" class="h-6 ficon" />
+          </a>
+          <a href="#">
+            <img src="../assets/bookmark/images/icon-twitter.svg" alt="" class="h-6 ficon" />
+          </a>>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
