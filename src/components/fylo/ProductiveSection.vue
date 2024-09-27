22f3f23c8b4c7b4f9d5e2bef2ce7f72921e99ca4
Yoko Harada <yokolet@gmail.com>
2024-09-27 15:39:02 +0900

    moves fylo sections into own components

---
+++ src/components/fylo/ProductiveSection.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <!-- Productive Section -->
+  <section id="productive" class="bg-white dark:bg-darkBlue">
+    <!-- Productive Container -->
+    <div class="container flex flex-col items-center mx-auto px-6 pt-24 pb-32 md:flex-row md:space-x-16">
+      <!-- Image -->
+      <div class="md:w-1/2">
+        <img src="/src/assets/fylo/images/illustration-stay-productive.png" alt="" class="mb-10">
+      </div>
+      <!-- Content -->
+      <div class="flex flex-col items-start md:w-1/2">
+        <div class="flex flex-col space-y-5">
+          <h4 class="max-w-md text-xl font-bold md:text-4xl">
+            Stay productive, where you are
+          </h4>
+          <p class="text-md md:text-lg">
+            Never let location be an issue when accessing your files. Fylo has you covered for all of your
+            file storage needs.
+          </p>
+          <p class="text-md md:text-lg">
+            Securely share files and folders with friends, family, and colleagues for live collaboration.
+            No email attachments required.
+          </p>
+        </div>
+        <!-- Link -->
+        <div class="block mt-4">
+          <a href="#" class="border-b order-accentCyan text-accentCyan"
+          >
+            See how Fylo works
+            <img src="/src/assets/fylo/images/icon-arrow.svg" alt="" class="inline pb-2">
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/fylo/TestimonialSection.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import profileImage1 from '/src/assets/fylo/images/profile-1.jpg';
+import profileImage2 from '/src/assets/fylo/images/profile-2.jpg';
+import profileImage3 from '/src/assets/fylo/images/profile-3.jpg';
+
+interface ICustomer {
+  image: string;
+  description: string;
+  name: string;
+  company: string;
+}
+
+const customers = ref<ICustomer[]>([
+  {
+    image: profileImage1,
+    description: 'Fylo has improved our team productivity by an order of magnitude. ' +
+        'Since making the switch our team has become a well-oiled collaboration machine.',
+    name: 'Satish Patel',
+    company: 'Founder & CEO. Huddle',
+  },
+  {
+    image: profileImage2,
+    description: 'Fylo has improved our team productivity by an order of magnitude. ' +
+        'Since making the switch our team has become a well-oiled collaboration machine.',
+    name: 'Bruce McKenzie',
+    company: 'Founder & CEO. Huddle 2',
+  },
+  {
+    image: profileImage3,
+    description: 'Fylo has improved our team productivity by an order of magnitude. ' +
+        'Since making the switch our team has become a well-oiled collaboration machine.',
+    name: 'Eva Boyd',
+    company: 'Founder & CEO. Huddle 3',
+  },
+]);
+</script>
+
+<template>
+  <!-- Testimonials Section -->
+  <section id="testimonials" class="bg-gray-50 dark:bg-darkBlue">
+    <!-- Testimonial Container -->
+    <div class="container mx-auto px-6 pt-12 pb-80 md:pb-96">
+      <!-- Boxes Container -->
+      <div class="relative flex flex-col w-full space-y-6 md:flex-row md:space-y-0 md:space-x-12">
+        <!-- Quote Image -->
+        <img
+            src="/src/assets/fylo/images/bg-quotes.png"
+            alt=""
+            class="absolute left-1 -top-2 w-10 md:-top-16 md:w-20" />
+        <!-- Boxes -->
+        <div
+            v-for="customer in customers"
+            class="flex flex-col p-10 space-y-6 rounded-lg bg-gray-100 dark:bg-darkBlue3 md:2-1/3"
+        >
+          <p class="text-sm leading-5 md:text-lg">
+            {{ customer.description }}
+          </p>
+          <!-- Customer Info -->
+          <div class="flex space-x-4">
+            <img :src="customer.image" alt="" class="w-10 h-10 rounded-full">
+          </div>
+          <div>
+            <h5 class="text-sm font-semibold">{{ customer.name }}</h5>
+            <p class="text-xs font-extralight">{{ customer.company }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
